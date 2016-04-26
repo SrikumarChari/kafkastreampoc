780b4d500cc43ad13e4e28a3834b2ccb773728be
Srikumar Chari <SrikumarChari@users.noreply.github.com>
2016-04-25 18:48:09 -0700

    initial checkin

---
+++ kafkastreamspoc/src/main/java/com/tfs/poc/kafkastreams/KafkaStreamsPoc.java
@@ -0,0 +1,16 @@
+package com.tfs.poc.kafkastreams;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Created by srikumar.chari on 4/25/16.
+ */
+public class KafkaStreamsPoc {
+
+    public static Logger logger = LogManager.getLogger(KafkaStreamsPoc.class.getSimpleName());
+
+    public static void main (char[] args) {
+
+    }
+}
